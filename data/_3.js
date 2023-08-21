bd78e0193fb7fc5cf96a1b7df294ff1e18c1ee14
furqan2024 <142742066+furqan2024@users.noreply.github.com>
2023-08-21 15:10:55 +0300

    Add files via upload

---
+++ data/_3.js
@@ -0,0 +1,1 @@
+var json__3 = {"type":"FeatureCollection","name":"_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","name":"شباك التسبيل"},"geometry":{"type":"Point","coordinates":[31.244764622819027,30.032263886679626]}}]}
